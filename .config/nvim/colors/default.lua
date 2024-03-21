325950baa97c06ee1a932ed2d022bb443a4971bf
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-03-21 20:52:09 +0100

    chore(nvim): use as much nvim default colorscheme as possible

--- .config/nvim/colors/default.lua
+++ .config/nvim/colors/default.lua
@@ -5,21 +5,24 @@ vim.g.colors_name = "default"
 vim.o.background = "dark"
 vim.o.termguicolors = true
 
-local palette = {
-  red = "#C94F6D",
-  blue = "#719CD6",
-  cyan = "#63CDCF",
-}
+---https://github.com/neovim/neovim/pull/26540#issue-2038341661
+---https://github.com/neovim/neovim/issues/26857
+local bg = vim.o.background == "dark" and "NvimDark" or "NvimLight"
+local fg = bg == "NvimDark" and "NvimLight" or "NvimDark"
 
 local groups = {
-  StatusLine = { link = "CursorLine" },
+  ---Builtin
+  DiffChange = { bg = bg .. "Grey3" },
   ---LSP
   LspCodeLens = { link = "Comment" },
   ---DAP
-  DapBreakpoint = { fg = palette.red },
-  DapLogPoint = { fg = palette.blue },
-  DapStopped = { fg = palette.cyan },
+  DapBreakpoint = { fg = fg .. "Red" },
+  DapLogPoint = { fg = fg .. "Blue" },
+  DapStopped = { fg = fg .. "Cyan" },
 }
+
+vim.g.terminal_color_0 = vim.g.terminal_color_0 or (bg .. "Grey1")
+vim.g.terminal_color_8 = vim.g.terminal_color_8 or (bg .. "Grey1")
 
 for group, opts in pairs(groups) do
   vim.api.nvim_set_hl(0, group, opts)
