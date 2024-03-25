4ce66850effcaae9897c04d6d3562c7194f0b596
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-03-25 01:25:03 +0100

    feat(nvim): improve tsc-watch overseer custom task
    The task now works with monorepo setups

--- .config/nvim/lua/overseer/template/vscode-tasks/tsc_watch.lua
+++ .config/nvim/lua/overseer/template/vscode-tasks/tsc_watch.lua
@@ -1,15 +1,30 @@
 local utils = require("serranomorante.utils")
 if not utils.is_available("overseer.nvim") then return {} end
 local overseer = require("overseer")
+local typescript_provider = require("overseer.template.vscode.provider.typescript")
 
 return {
-  name = "tsc: watch - tsconfig.json",
+  name = "tsc: watch - relative tsconfig.json",
+  desc = "Watch a typescript project using the tsconfig.json from the current buffer's directory",
   builder = function()
+    local tsconfig = vim.fs.find("tsconfig.json", {
+      stop = vim.fn.getcwd() .. "/..",
+      type = "file",
+      upward = true,
+      path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
+    })
+
+    if vim.tbl_count(tsconfig) == 0 then return vim.notify("tsconfig.json not found", vim.log.levels.ERROR) end
+
+    local task_opts = typescript_provider.get_task_opts({
+      tsconfig = tsconfig[1],
+      option = "watch",
+    })
+
     return {
-      cmd = { "tsc", "--watch" },
+      cmd = task_opts.cmd,
       components = {
         { "on_output_parse", problem_matcher = "$tsc-watch" },
-        "on_result_diagnostics",
         "on_result_diagnostics_quickfix",
         "default",
       },
@@ -17,6 +32,6 @@ return {
   end,
   tags = { overseer.TAG.BUILD },
   condition = {
-    filetype = { "typescript" },
+    filetype = { "typescript", "typescriptreact" },
   },
 }
