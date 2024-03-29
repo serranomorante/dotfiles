83a63fec4256889f0c896e712518557eb4c17d3e
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-03-29 21:55:30 +0100

    chore(nvim): disable copilot on oil buffers

--- .config/nvim/lua/serranomorante/plugins/copilot.lua
+++ .config/nvim/lua/serranomorante/plugins/copilot.lua
@@ -4,6 +4,9 @@ return {
   init = function()
     vim.g.copilot_node_command = vim.g.node_system_executable
     vim.g.copilot_no_tab_map = true -- I use `tab` for completion
+    vim.g.copilot_filetypes = {
+      ["oil"] = false,
+    }
   end,
   config = function()
     vim.keymap.set("i", "<M-c>", 'copilot#Accept("\\<CR>")', {
