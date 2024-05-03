701dc14bc04968a69a7cbb767e6c71fba55bf7fa
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-05-04 01:50:03 +0200

    feat(nvim): add breadcrumb winbar
    Add aerial

--- .config/nvim/lua/serranomorante/plugins/statusline/heirline.lua
+++ .config/nvim/lua/serranomorante/plugins/statusline/heirline.lua
@@ -1,9 +1,25 @@
 return {
   "rebelot/heirline.nvim",
   event = "UiEnter",
+  dependencies = {
+    "stevearc/aerial.nvim",
+    opts = {
+      filter_kind = false,
+      disable_max_lines = vim.g.max_file.lines,
+      disable_max_size = vim.g.max_file.size,
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "Re-trigger disable_winbar_cb on attach",
+      group = vim.api.nvim_create_augroup("retrigger_disable_winbar_cb_on_attach", { clear = true }),
+      callback = function() vim.api.nvim_exec_autocmds("Filetype", { group = "Heirline_init_winbar" }) end,
+    })
+  end,
   opts = function()
     local conditions = require("heirline.conditions")
     local components = require("serranomorante.plugins.statusline.components")
+    local winbar_components = require("serranomorante.plugins.statusline.winbar-components")
 
     local DefaultStatusLine = {
       components.Mode,
@@ -52,9 +68,11 @@ return {
       fallthrough = false,
       {
         condition = function() return conditions.buffer_matches({ filetype = { "oil" } }) end,
-        {
-          provider = function() return vim.fn.fnamemodify(require("oil").get_current_dir(), ":.") end,
-        },
+        winbar_components.Oil,
+      },
+      {
+        condition = function() return conditions.lsp_attached end,
+        winbar_components.Breadcrumb,
       },
     }
 
@@ -62,8 +80,11 @@ return {
       statusline = StatusLines,
       winbar = WinBars,
       opts = {
-        ---Only enable winbar on oil.nvim buffers
-        disable_winbar_cb = function(args) return not conditions.buffer_matches({ filetype = { "oil" } }, args.buf) end,
+        disable_winbar_cb = function(args)
+          if conditions.buffer_matches({ filetype = { "oil" } }, args.buf) then return false end
+          if vim.tbl_count(vim.lsp.get_clients({ bufnr = args.buf })) > 0 then return false end
+          return true
+        end,
       },
     }
   end,
---
+++ .config/nvim/lua/serranomorante/plugins/statusline/winbar-components.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+M.Breadcrumb = {
+  condition = function() return package.loaded["aerial"] end,
+  update = { "CursorMoved", "LspAttach" },
+  init = function(self)
+    local data = require("aerial").get_location(true) or {}
+    local children = {}
+    local max_depth = 5
+    local separator = " > "
+
+    local start_idx = 0
+    if max_depth > 0 then
+      start_idx = #data - max_depth
+      if start_idx > 0 then table.insert(children, {
+        provider = "…" .. separator,
+      }) end
+    end
+
+    for i, d in ipairs(data) do
+      if i > start_idx then
+        ---Symbol name
+        local child = {
+          {
+            provider = string.gsub(d.name, "%%", "%%%%"):gsub("%s*->%s*", ""),
+          },
+        }
+        ---Icon
+        table.insert(child, 1, {
+          provider = string.format("%s ", d.icon),
+        })
+        if #data > 1 and i < #data then table.insert(child, { provider = separator }) end
+        table.insert(children, child)
+      end
+    end
+
+    self[1] = self:new(children, 1)
+  end,
+}
+
+M.Oil = {
+  provider = function() return vim.fn.fnamemodify(require("oil").get_current_dir(), ":.") end,
+}
+
+return M
