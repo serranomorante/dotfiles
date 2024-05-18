72c7c8993e215a8c354d2d07f9600b2fd5811433
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-05-18 17:45:33 +0200

    fix(fish): sort fzf results when searching commandline history

--- .config/fish/functions/reverse_history_search.fish
+++ .config/fish/functions/reverse_history_search.fish
@@ -1,6 +1,6 @@
 # Use fzf to search history
 function reverse_history_search
-    history | fzf --no-sort -q "$(commandline -b)" | read -l command
+    history | fzf -q "$(commandline -b)" | read -l command
     if test $command
         commandline -rb $command
     end
