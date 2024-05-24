03b6a4177a165a5163c425d0a5b1000eebfe89b0
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-05-24 23:45:01 +0200

    chore(nvim): improve undotree defaults

--- .config/nvim/lua/serranomorante/plugins/undotree.lua
+++ .config/nvim/lua/serranomorante/plugins/undotree.lua
@@ -1,6 +1,15 @@
 return {
   "mbbill/undotree",
   lazy = true,
+  init = function()
+    ---undotree window width
+    vim.g.undotree_SplitWidth = 80
+    ---if set, let undotree window get focus after being opened, otherwise
+    ---focus will stay in current window.
+    vim.g.undotree_SetFocusWhenToggle = 1
+    ---auto open diff window
+    vim.g.undotree_DiffAutoOpen = 0
+  end,
   keys = {
     {
       "<leader>uu",
