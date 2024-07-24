b8bf59eb6839e85e83196c24f98a61f11e24c92d
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-07-24 22:25:27 +0200

    feat(nvim): add gitignore logic to oil.nvim

--- .config/nvim/lua/serranomorante/plugins/oil.lua
+++ .config/nvim/lua/serranomorante/plugins/oil.lua
@@ -1,3 +1,25 @@
+---https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#hide-gitignored-files
+local git_ignored = setmetatable({}, {
+  __index = function(self, key)
+    local proc = vim.system({ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" }, {
+      cwd = key,
+      text = true,
+    })
+    local result = proc:wait()
+    local ret = {}
+    if result.code == 0 then
+      for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+        ---Remove trailing slash
+        line = line:gsub("/$", "")
+        table.insert(ret, line)
+      end
+    end
+
+    rawset(self, key, ret)
+    return ret
+  end,
+})
+
 return {
   "stevearc/oil.nvim",
   lazy = false,
@@ -47,7 +69,14 @@ return {
 
     return {
       view_options = {
-        show_hidden = true,
+        show_hidden = false,
+        is_hidden_file = function(name, _)
+          local dir = require("oil").get_current_dir()
+          ---if no local directory (e.g. for ssh connections), always show
+          if not dir then return false end
+          ---Check if file is gitignored
+          return vim.list_contains(git_ignored[dir], name)
+        end,
       },
       ---https://github.com/stevearc/oil.nvim/issues/201#issuecomment-1771146785
       cleanup_delay_ms = false,
