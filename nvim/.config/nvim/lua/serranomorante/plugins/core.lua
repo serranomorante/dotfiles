ff36315650b2f1be7f7560bfaf8c655148f4427a
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-08-08 20:53:55 +0200

    chore(nvim): remove unused code

--- nvim/.config/nvim/lua/serranomorante/plugins/core.lua
+++ nvim/.config/nvim/lua/serranomorante/plugins/core.lua
@@ -1,15 +1,5 @@
 return {
-  { "nvim-lua/plenary.nvim", lazy = true },
+  { "nvim-lua/plenary.nvim", cmd = "PlenaryBustedDirectory" },
   { "kevinhwang91/promise-async", lazy = true },
-  {
-    "nvim-tree/nvim-web-devicons",
-    lazy = true,
-    init = function()
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        desc = "Reset nvim-web-devicons highlight groups on ColorScheme",
-        group = vim.api.nvim_create_augroup("nvim-web-devicons-group", { clear = true }),
-        callback = function() require("nvim-web-devicons").setup({}) end,
-      })
-    end,
-  },
+  { "nvim-tree/nvim-web-devicons", lazy = true },
 }
