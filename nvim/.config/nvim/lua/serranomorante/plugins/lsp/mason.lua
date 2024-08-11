920ed9b8b3cb2dc77d781f51981add29961ffbb0
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-08-11 16:55:31 +0200

    feat(ansible): add yaml tools

--- nvim/.config/nvim/lua/serranomorante/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/serranomorante/plugins/lsp/mason.lua
@@ -39,7 +39,6 @@ return {
         tools.by_filetype.go,
         tools.by_filetype.bash,
         tools.by_filetype.vim,
-        tools.by_filetype.yaml,
         tools.by_filetype.rust,
         tools.by_filetype.toml,
         tools.by_filetype.python,
