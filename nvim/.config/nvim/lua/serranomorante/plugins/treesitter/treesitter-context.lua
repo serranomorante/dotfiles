a0442e88e3f3ea33b097af59105f3b04f51c46e8
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-08-25 22:11:39 +0200

    chore(nvim): disable treesitter context on large buffers

--- nvim/.config/nvim/lua/serranomorante/plugins/treesitter/treesitter-context.lua
+++ nvim/.config/nvim/lua/serranomorante/plugins/treesitter/treesitter-context.lua
@@ -4,6 +4,10 @@ local opts = function()
   return {
     max_lines = 1,
     trim_scope = "inner", -- outer
+    on_attach = function(bufnr)
+      if vim.b[bufnr].large_buf then return false end
+      return nil
+    end,
   }
 end
 
