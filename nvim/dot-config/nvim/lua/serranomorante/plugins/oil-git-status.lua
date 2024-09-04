77c34837e9f7d244a0b4bb0bece648b90e3774b4
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-09-04 22:56:15 +0200

    chore(nvim): add oil-git-status plugin

---
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/oil-git-status.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.config = function() require("oil-git-status").setup() end
+
+return M
--- nvim/dot-config/nvim/lua/serranomorante/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/oil.lua
@@ -80,6 +80,9 @@ M.config = function()
   local oil_actions = require("oil.actions")
   local opts = {
     watch_for_changes = true,
+    win_options = {
+      signcolumn = "yes:2",
+    },
     view_options = {
       show_hidden = show_hidden,
       is_hidden_file = function(name, _)
