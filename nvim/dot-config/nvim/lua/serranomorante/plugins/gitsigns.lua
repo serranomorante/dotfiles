d196b8df0f1563bd343d1a9e2aac0eee04283c55
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-09-04 22:57:50 +0200

    chore(nvim): improve repeatable behaviour for textobjects move and gitsigns

--- nvim/dot-config/nvim/lua/serranomorante/plugins/gitsigns.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/gitsigns.lua
@@ -9,29 +9,23 @@ local opts = function()
 end
 
 local keys = function()
-  vim.keymap.set("n", "<leader>gl", function() require("gitsigns").blame_line() end, { desc = "Git: Blame line" })
-  vim.keymap.set(
-    "n",
-    "<leader>gL",
-    function() require("gitsigns").blame_line({ full = true }) end,
-    { desc = "Git: Blame full buffer" }
-  )
-  vim.keymap.set("n", "<leader>gd", function() require("gitsigns").diffthis() end, { desc = "Git: Diff this" })
-  vim.keymap.set("n", "<leader>gp", function() require("gitsigns").preview_hunk() end, { desc = "Git: Preview hunk" })
-  vim.keymap.set("n", "]g", function() require("gitsigns").next_hunk() end, { desc = "Git: Next hunk" })
-  vim.keymap.set("n", "[g", function() require("gitsigns").prev_hunk() end, { desc = "Git: Prev hunk" })
-  vim.keymap.set("n", "<leader>gh", function() require("gitsigns").reset_hunk() end, { desc = "Git: Reset hunk" })
+  local gs = require("gitsigns")
+  local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+  local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
+
+  vim.keymap.set("n", "<leader>gl", function() gs.blame_line() end, { desc = "Git: Blame line" })
+  vim.keymap.set("n", "<leader>gL", function() gs.blame_line({ full = true }) end, { desc = "Git: Blame full buffer" })
+  vim.keymap.set("n", "<leader>gd", function() gs.diffthis() end, { desc = "Git: Diff this" })
+  vim.keymap.set("n", "<leader>gp", function() gs.preview_hunk() end, { desc = "Git: Preview hunk" })
+  vim.keymap.set({ "n", "x", "o" }, "]g", next_hunk_repeat, { desc = "Git: Next hunk" })
+  vim.keymap.set({ "n", "x", "o" }, "[g", prev_hunk_repeat, { desc = "Git: Prev hunk" })
+  vim.keymap.set("n", "<leader>gh", function() gs.reset_hunk() end, { desc = "Git: Reset hunk" })
   vim.keymap.set({ "n", "v" }, "<leader>gh", ":Gitsigns reset_hunk<CR>", { desc = "Git: Reset hunk (partial)" })
-  vim.keymap.set("n", "<leader>gH", function() require("gitsigns").reset_buffer() end, { desc = "Git: Reset buffer" })
-  vim.keymap.set("n", "<leader>gS", function() require("gitsigns").stage_buffer() end, { desc = "Git: Stage buffer" })
-  vim.keymap.set("n", "<leader>gs", function() require("gitsigns").stage_hunk() end, { desc = "Git: Stage hunk" })
+  vim.keymap.set("n", "<leader>gH", function() gs.reset_buffer() end, { desc = "Git: Reset buffer" })
+  vim.keymap.set("n", "<leader>gS", function() gs.stage_buffer() end, { desc = "Git: Stage buffer" })
+  vim.keymap.set("n", "<leader>gs", function() gs.stage_hunk() end, { desc = "Git: Stage hunk" })
   vim.keymap.set({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Git: Stage hunk (partial)" })
-  vim.keymap.set(
-    "n",
-    "<leader>gu",
-    function() require("gitsigns").undo_stage_hunk() end,
-    { desc = "Git: Unstage hunk" }
-  )
+  vim.keymap.set("n", "<leader>gu", function() gs.undo_stage_hunk() end, { desc = "Git: Unstage hunk" })
 end
 
 M.config = function()
--- nvim/dot-config/nvim/lua/serranomorante/plugins/treesitter/treesitter-context.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/treesitter/treesitter-context.lua
@@ -11,6 +11,19 @@ local opts = function()
   }
 end
 
-M.config = function() require("treesitter-context").setup(opts()) end
+M.config = function()
+  require("treesitter-context").setup(opts())
+
+  ---https://github.com/nvim-treesitter/nvim-treesitter-textobjects?tab=readme-ov-file#text-objects-move
+  local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+  vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+  vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+  ---Get the default behaviour again (that behaviour overrided by ts_repeat_move.repeat_last_move)
+  vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+  vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+  vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+  vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+end
 
 return M
