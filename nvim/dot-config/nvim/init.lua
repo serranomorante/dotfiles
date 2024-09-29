a70a1813c839766c9835a92e187ec800ef474681
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-09-29 23:57:30 +0200

    chore(nvim): add new filetypes map

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -3,9 +3,14 @@ vim.loader.enable()
 vim.filetype.add({
   filename = {
     ["sxhkdrc"] = "sxhkdrc",
+    [".stow-global-ignore"] = "gitignore",
+    [".stow-local-ignore"] = "gitignore",
+    ["dot-bashrc"] = "bash",
+    ["xhtml"] = "html",
   },
   pattern = {
     [".*/requirements.*%.txt"] = "requirements",
+    ["^%.env.*"] = "bash",
   },
 })
 
