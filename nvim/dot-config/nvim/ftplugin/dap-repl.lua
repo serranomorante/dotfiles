bc70c51f5003516d99729b250003576cbc184f7e
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-10-12 12:15:53 +0200

    chore(nvim): improve dap-repl bg color implementation

--- nvim/dot-config/nvim/ftplugin/dap-repl.lua
+++ nvim/dot-config/nvim/ftplugin/dap-repl.lua
@@ -1,3 +1,20 @@
-local dap_repl_hl = vim.api.nvim_create_namespace("dap-repl-hl")
-vim.api.nvim_set_hl(dap_repl_hl, "Normal", { bg = "NvimDarkGray3" })
-vim.api.nvim_win_set_hl_ns(0, dap_repl_hl)
+local repl_ns = vim.api.nvim_create_namespace("dap-repl-hl")
+local group = vim.api.nvim_create_augroup("dap-repl-hl", { clear = true })
+local buffer = vim.api.nvim_get_current_buf()
+
+vim.api.nvim_create_autocmd({ "BufEnter", "TabEnter", "WinEnter" }, {
+  desc = "Change window background color for dap-repl",
+  buffer = buffer,
+  group = group,
+  callback = function()
+    vim.api.nvim_set_hl(repl_ns, "Normal", { bg = "#192335" })
+    vim.api.nvim_win_set_hl_ns(0, repl_ns)
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  desc = "Reset window background color when leaving dap-repl",
+  buffer = buffer,
+  group = group,
+  callback = vim.schedule_wrap(function() vim.api.nvim_win_set_hl_ns(0, 0) end),
+})
