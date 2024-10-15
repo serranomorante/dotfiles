747af259db93d8fdee1f3cf54653d2362b80127e
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-10-15 23:50:45 +0200

    chore(nvim): undotree reduce width and show diff

--- nvim/dot-config/nvim/lua/serranomorante/plugins/undotree.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/undotree.lua
@@ -2,12 +2,10 @@ local M = {}
 
 local init = function()
   ---undotree window width
-  vim.g.undotree_SplitWidth = 80
+  vim.g.undotree_SplitWidth = 30
   ---if set, let undotree window get focus after being opened, otherwise
   ---focus will stay in current window.
   vim.g.undotree_SetFocusWhenToggle = 1
-  ---auto open diff window
-  vim.g.undotree_DiffAutoOpen = 0
 end
 
 local keys = function() vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" }) end
