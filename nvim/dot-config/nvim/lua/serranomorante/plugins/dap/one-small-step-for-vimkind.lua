f891ecbf937b85862e5fe6f6509b347cc6ffb560
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-12-31 16:38:33 +0100

    fix(nvim): osv --listen issue fixed upstream

--- nvim/dot-config/nvim/lua/serranomorante/plugins/dap/one-small-step-for-vimkind.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/dap/one-small-step-for-vimkind.lua
@@ -1,18 +1,17 @@
 local M = {}
 
 local keys = function()
-  vim.keymap.set("n", "<leader>cc", function()
-    if vim.env.CUSTOM_NVIM_LISTEN_ADDRESS == vim.v.servername then
-      ---osv uses `nvim --headless --embed` so we cannot start neovim with `--listen`
-      ---https://github.com/jbyuki/one-small-step-for-vimkind/issues/14#issuecomment-1105938997
-      return vim.notify("Please start nvim without any arguments", vim.log.levels.ERROR)
-    end
-
-    require("osv").launch({
-      port = 8086,
-      log = vim.env.DAP_LOG_LEVEL == "TRACE",
-    })
-  end, { desc = "DAP: Launch one small step for vimkind" })
+  vim.keymap.set(
+    "n",
+    "<leader>cc",
+    function()
+      require("osv").launch({
+        port = 8086,
+        log = vim.env.DAP_LOG_LEVEL == "TRACE",
+      })
+    end,
+    { desc = "DAP: Launch one small step for vimkind" }
+  )
 end
 
 M.config = function()
