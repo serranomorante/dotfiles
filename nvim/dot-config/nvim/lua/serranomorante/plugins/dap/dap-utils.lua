f5cd43d1bc80657e86cf0f9dd218a3d08ee34b05
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-03-21 15:35:15 +0100

    fix(nvim): webRoot not working on non-monorepo projects

--- nvim/dot-config/nvim/lua/serranomorante/plugins/dap/dap-utils.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/dap/dap-utils.lua
@@ -27,10 +27,10 @@ function M.pick_workspace_relative_to_file(files)
     local items = vim.tbl_map(function(path)
       local dirname = vim.fn.fnamemodify(path, ":p:h"):sub(#workspaceFolder + 1, -1)
       if dirname == "" then return workspaceFolder end
-      return dirname
+      return workspaceFolder .. dirname
     end, paths)
     vim.ui.select(items, { label = workspaceFolder }, function(choice)
-      if choice then coroutine.resume(dap_run_co, workspaceFolder .. choice) end
+      if choice then coroutine.resume(dap_run_co, choice) end
     end)
   end)
 end
