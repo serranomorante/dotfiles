bd089d20a3e74867feb287ba0d7089b667b64022
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-03-23 17:45:45 +0100

    promnesia exclusion doesn't work

--- PKM/dot-config/promnesia/config.py
+++ PKM/dot-config/promnesia/config.py
@@ -1,4 +1,4 @@
-from promnesia.common import Source
+from promnesia.common import Source, DbVisit, get_logger
 from promnesia.sources import auto, browser
 from promnesia.sources import hypothesis
 
@@ -23,9 +23,28 @@ SOURCES = [
             "*.bib",
             "*.lock",
             "*.scss",
+            "*/tags",
+            "*/mermaid-filter.err",
             "*/Gemfile",
             "*/assets/*",
         ],
         name="notes",
     ),
 ]
+
+# https://github.com/karlicoss/promnesia/blob/master/doc/config.py
+FILTERS = [
+    "developer.wikimedia.org"  # exclude my own default new tab page
+]
+
+
+# https://github.com/karlicoss/promnesia/blob/master/doc/config.py
+def HOOK(v: DbVisit):
+    logger = get_logger()
+
+    logger.info("{}".format(v))
+
+    if hasattr(v, "orig_url") and "developer.wikimedia.org" in v.orig_url:
+        logger.info("Match! {}".format(v.orig_url))
+        return
+    yield v
