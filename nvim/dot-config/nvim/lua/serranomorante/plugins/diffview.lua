bfaaae133694921bf9c6eeaafc49a04037b9811a
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-04-03 20:09:20 +0200

    feat(nvim): add keymap to search on diffview file history

--- nvim/dot-config/nvim/lua/serranomorante/plugins/diffview.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/diffview.lua
@@ -4,6 +4,7 @@ local FILE_HISTORY_PANEL_HEIGHT = 6
 
 local keys = function()
   vim.keymap.set("n", "<leader>vd", "<cmd>DiffviewOpen<CR>", { desc = "Diffview: compare against current index" })
+  vim.keymap.set("n", "<leader>vz", ":DiffviewFileHistory -S=", { desc = "Diffview: search in git history" })
   vim.keymap.set(
     "n",
     "<leader>vf",
