4e86e680901020f2d87af892d457cbaa70459520
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-04-10 20:05:11 +0200

    chore(nvim): force print even on silenced messages

--- nvim/dot-config/nvim/lua/serranomorante/globals.lua
+++ nvim/dot-config/nvim/lua/serranomorante/globals.lua
@@ -1,4 +1,2 @@
-P = function(v)
-  vim.print(vim.inspect(v))
-  return v
-end
+---https://github.com/stevearc/resession.nvim/issues/44#issuecomment-2027345600
+P = function(v) vim.cmd.echom({ args = { vim.fn.string(vim.inspect(v)) }, mods = { unsilent = true } }) end
