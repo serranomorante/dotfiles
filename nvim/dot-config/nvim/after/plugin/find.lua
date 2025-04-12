11a185c817240853c06d5c31a36e87bb1dd5043f
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-04-12 09:52:58 +0200

    chore(nvim): enhance find plugin

--- nvim/dot-config/nvim/after/plugin/find.lua
+++ nvim/dot-config/nvim/after/plugin/find.lua
@@ -1,18 +1,28 @@
+---You must escape pipes `:Find 'file(-\|_)one' ...`
+
+---Send find results to quickfix list
 ---@param items string[]
 local function send_to_qf(items)
   local files = vim.tbl_map(function(item) return { filename = item } end, items)
   vim.fn.setqflist({}, "r", { title = "Find results", items = files or {} })
 end
 
-local function complete(arg_lead)
-  local result = vim.fn.system("fd --type file --follow --full-path " .. arg_lead)
-  return vim.split(result or "", "\n", { trimempty = true })
+---Use `fd` to perform the file and directory search
+---@param arg_lead string
+local function find(arg_lead)
+  arg_lead = arg_lead:gsub("\\|", "|") -- fix regex pipe compatibility
+  return vim.split(vim.fn.system("fd --type file --follow --full-path " .. arg_lead) or "", "\n", { trimempty = true })
 end
 
-vim.api.nvim_create_user_command("Find", "find <args>", { force = true, nargs = "?", complete = complete })
+vim.api.nvim_create_user_command(
+  "Find",
+  "find <args>",
+  { force = true, nargs = "*", complete = "file", desc = "Send find to qf list (even if zero results)" }
+)
 
+---@param cmd_arg string
 _G.user.findfunc = function(cmd_arg)
-  local files = complete(cmd_arg)
+  local files = find(cmd_arg)
   vim.schedule(function() send_to_qf(files) end)
   return files
 end
--- nvim/dot-config/nvim/after/plugin/grep.lua
+++ nvim/dot-config/nvim/after/plugin/grep.lua
@@ -3,11 +3,11 @@
 vim.api.nvim_create_user_command(
   "Grep",
   "silent execute v:lua.user.GenerateGrepCommand(<q-args>)",
-  { force = true, nargs = "*", complete = "file" }
+  { force = true, nargs = "*", complete = "file", desc = "Send grep to qf list (even if zero results)" }
 )
 
 ---@param args string
 _G.user.GenerateGrepCommand = function(args)
   args = args:gsub("\\'", "\\x27") -- escape single quotes
-  return string.format("grep -e %s", args)
+  return "grep -e " .. args
 end
