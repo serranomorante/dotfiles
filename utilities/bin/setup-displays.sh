45bf4aec8db6e6ff2f33bb4995f76a2a7f95fe12
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-07-24 21:04:05 +0200

    chore(ansible): use nvidia graphics only
    [](https://wiki.archlinux.org/title/NVIDIA_Optimus#Use_NVIDIA_graphics_only)

--- utilities/bin/setup-displays.sh
+++ utilities/bin/setup-displays.sh
@@ -9,14 +9,17 @@ if [[ $external == "" ]]; then
    external=$(xrandr | grep "DP.* connected" | cut -d " " -f 1 | sed -n '2p') # try a second match
 fi
 
-xrandr --output $internal --primary --mode 1920x1080 --rate 120.21 --brightness 0.5
+xrandr --newmode "1920x1080f"   285.00   1920 2028 2076 2076   1080 1090 1100 1142 -hsync -vsync
+xrandr --addmode $internal "1920x1080f"
+xrandr --output $internal --primary --mode "1920x1080f" --rate 120.21
+# xrandr --output $internal --set "PRIME Synchronization" 1
 
 if [[ $external != "" ]]; then
    xrandr --output $external --mode 3440x1440 --rate 59.97 --right-of $internal --brightness 0.5
    # Notify the system
    /usr/bin/notify-send "[external] $external connected"
    # Under-clock the graphics memory to save energy
-   nvidia-settings -a "GPUMemoryTransferRateOffset[0x3]=-2000" -c $DISPLAY
+   # nvidia-settings -a "GPUMemoryTransferRateOffset[0x3]=-2000" -c $DISPLAY
 else
    external=$(xrandr --listmonitors | grep -Po "\d:\s\+\K[^\s*]+")
    if [[ $external != "" ]]; then
