04b57865ad4cf19e89f9b5a334c081258b8974ae
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-08-29 19:58:54 +0200

    chore(nvim): update config to upstream changes

--- nvim/dot-config/nvim/lua/serranomorante/plugins/codecompanion.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/codecompanion.lua
@@ -43,36 +43,38 @@ local function opts()
     diff = {
       enabled = false,
     },
-    adapters = {
-      gemini = function()
-        return require("codecompanion.adapters").extend("gemini", {
-          env = {
-            api_key = "cmd: gpg --decrypt ~/secrets/gemini_api_key.gpg 2>/dev/null",
-          },
-        })
-      end,
-      openai = function()
-        return require("codecompanion.adapters").extend("openai", {
-          opts = {
-            stream = true,
-          },
-          env = {
-            api_key = "cmd: gpg --decrypt ~/openai_api_key.asc 2>/dev/null",
-          },
-          schema = {
-            model = {
-              default = function() return "gpt-4.1" end,
+    http = {
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = "cmd: gpg --decrypt ~/secrets/gemini_api_key.gpg 2>/dev/null",
             },
-          },
-        })
-      end,
-      anthropic = function()
-        return require("codecompanion.adapters").extend("anthropic", {
-          env = {
-            api_key = "cmd: gpg --decrypt ~/secrets/anthropic_api_key.gpg 2>/dev/null",
-          },
-        })
-      end,
+          })
+        end,
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            opts = {
+              stream = true,
+            },
+            env = {
+              api_key = "cmd: gpg --decrypt ~/openai_api_key.asc 2>/dev/null",
+            },
+            schema = {
+              model = {
+                default = function() return "gpt-4.1" end,
+              },
+            },
+          })
+        end,
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd: gpg --decrypt ~/secrets/anthropic_api_key.gpg 2>/dev/null",
+            },
+          })
+        end,
+      },
     },
     strategies = {
       chat = {
