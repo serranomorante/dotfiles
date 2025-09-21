9cc6122556a1c0229cafa1f6cd79e7771c3dde9a
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-09-21 23:11:49 +0200

    chore(nvim): add markdown title to files that don't have it

--- nvim/dot-config/nvim/after/ftplugin/markdown.lua
+++ nvim/dot-config/nvim/after/ftplugin/markdown.lua
@@ -3,6 +3,18 @@ local lsp_utils = require("serranomorante.plugins.lsp.utils")
 
 local bufnr = vim.api.nvim_get_current_buf()
 
+---Fill title to markdown files that don't have it
+if not select(1, unpack(vim.api.nvim_buf_get_lines(bufnr, 0, 1, false))):match("#") then
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  if vim.fn.filereadable(bufname) == 1 then
+    local f = vim.fn.split(vim.fn.fnamemodify(bufname, ":t"), "\\.")
+    table.remove(f) -- remove the `.md` part
+    local fd = assert(vim.uv.fs_open(bufname, "w", 420))
+    vim.uv.fs_write(fd, "# " .. vim.fn.join(vim.tbl_map(function(i) return i:gsub("^%l", string.upper) end, f), " | "))
+    vim.uv.fs_close(fd)
+  end
+end
+
 if coc_utils.should_enable(bufnr) then
   require("serranomorante.plugins.coc").start(nil, { bufnr = bufnr })
   if not vim.g.markdown_preview_loaded then
