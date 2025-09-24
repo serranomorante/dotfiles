2e6a44af45172f3bcffeb02818537c238fc770ae
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-09-24 08:59:34 +0200

    chore(nvim): don't autostart markdown preview

--- nvim/dot-config/nvim/after/ftplugin/markdown.lua
+++ nvim/dot-config/nvim/after/ftplugin/markdown.lua
@@ -17,10 +17,6 @@ end
 
 if coc_utils.should_enable(bufnr) then
   require("serranomorante.plugins.coc").start(nil, { bufnr = bufnr })
-  if not vim.g.markdown_preview_loaded then
-    vim.cmd("CocCommand markdown-preview-enhanced.openPreview")
-    vim.g.markdown_preview_loaded = true
-  end
 elseif lsp_utils.should_enable(bufnr) then
   vim.lsp.enable("marksman")
   vim.api.nvim_exec_autocmds("FileType", { group = "nvim.lsp.enable" })
