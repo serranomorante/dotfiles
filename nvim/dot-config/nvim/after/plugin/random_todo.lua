d148d12ff305173f5a5e8a9219e6da5f0eb9b351
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-09-28 21:22:56 +0200

    chore(nvim): prevent :RandomTodo to repeat ocurrences

--- nvim/dot-config/nvim/after/plugin/random_todo.lua
+++ nvim/dot-config/nvim/after/plugin/random_todo.lua
@@ -1,10 +1,19 @@
 local utils = require("serranomorante.utils")
 
+local cache = {}
 local function random_todo()
   local items, count = utils.rg_json_to_qfitems(utils.grep_with_rg("'^- \\[ \\]'", { json = true }))
   if count == 0 then return vim.api.nvim_echo({ { "No pending TODOs" } }, false, { err = true }) end
+  local random_count = math.random(count)
+  local cache_full = vim.tbl_count(cache) == vim.tbl_count(items)
+  while vim.list_contains(cache, items[random_count].text) and not cache_full do
+    random_count = math.random(count)
+    cache_full = vim.tbl_count(cache) == vim.tbl_count(items)
+  end
+  if cache_full then return vim.api.nvim_echo({ { "All random todos exhausted!" } }, false, { err = true }) end
+  table.insert(cache, items[random_count].text)
   vim.fn.setqflist({}, " ", { title = "Go to random TODO", items = items })
-  vim.cmd.cc(math.random(count + 1))
+  vim.cmd.cc({ args = { random_count }, mods = { silent = true } })
 end
 
 vim.api.nvim_create_user_command("RandomTodo", random_todo, { force = true, nargs = "*", desc = "Go to random TODO" })
