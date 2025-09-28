93bf07d35dc138bb12f536d99b5a880fb6b60cf6
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-09-28 22:44:59 +0200

    chore(nvim): use new relative_date_if_recent in blame.nvim

--- nvim/dot-config/nvim/lua/serranomorante/plugins/blame.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/blame.lua
@@ -2,9 +2,15 @@ local M = {}
 
 local function keys() vim.keymap.set("n", "<leader>lb", "<cmd>BlameToggle<CR>", { desc = "Blame: toggle blame" }) end
 
+local function opts()
+  return {
+    relative_date_if_recent = true,
+  }
+end
+
 function M.config()
   keys()
-  require("blame").setup()
+  require("blame").setup(opts())
 end
 
 return M
