473650b69859335a0cafe6dc6ce5ac002d1e95c5
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-10-01 16:59:44 +0200

    feat(ansible): add chrome-devtools mcp

--- nvim/dot-config/nvim/lua/serranomorante/plugins/mcphub.lua
+++ nvim/dot-config/nvim/lua/serranomorante/plugins/mcphub.lua
@@ -1,10 +1,19 @@
+local binaries = require("serranomorante.binaries")
+
 local M = {}
 
 local function init() vim.cmd([[cab ch MCPHub]]) end
 
+local function opts()
+  ---@type MCPHub.Config
+  return {
+    cmd = binaries.mcp_hub_executable(),
+  }
+end
+
 function M.config()
   init()
-  require("mcphub").setup()
+  require("mcphub").setup(opts())
 end
 
 return M
