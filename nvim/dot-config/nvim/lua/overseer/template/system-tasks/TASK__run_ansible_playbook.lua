6885c91d5effa6eddaade58ace0498496c1476f3
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-10-01 16:58:19 +0200

    chore(nvim): improve playbook task

--- nvim/dot-config/nvim/lua/overseer/template/system-tasks/TASK__run_ansible_playbook.lua
+++ nvim/dot-config/nvim/lua/overseer/template/system-tasks/TASK__run_ansible_playbook.lua
@@ -1,41 +1,35 @@
 local utils = require("serranomorante.utils")
 local task_name = "run-ansible-playbook"
 local HOME = vim.env.HOME
-local CWD = vim.fn.getcwd()
-local CHOICES = {
-  ---These choices will only show get cwd is assets
-  [HOME .. "/dotfiles/assets"] = {
-    "10-40-setup-keyboard-tools",
-    "10-120-setup-wine-tools",
-    "10-170-setup-browser-tools",
-    "10-180-setup-backup-tools,10-181-setup-root-backup-tools",
-  },
-  [HOME .. "/dotfiles/utilities"] = {
-    "10-100-setup-compositor",
-  },
-  [HOME .. "/dotfiles/for-my-eyes-only"] = {
-    "10-10-setup-desktop",
-    "60-10-for-my-eyes-only",
-    "60-20-for-my-eyes-only",
-    "60-30-for-my-eyes-only",
-    "60-40-for-my-eyes-only",
-    "70-10-for-my-eyes-only",
-  },
-  [HOME .. "/dotfiles/playbooks"] = {
-    "10-10-setup-desktop",
-    "10-30-setup-dotfiles",
-    "10-100-setup-compositor",
-    "10-120-setup-wine-tools",
-    "10-140-setup-virtualbox",
-    "10-170-setup-browser-tools",
-    "10-180-setup-backup-tools,10-181-setup-root-backup-tools",
-    "20-50-setup-neovim",
-    "20-60-setup-editor-tools",
-    "40-20-setup-HPI",
-    "40-30-setup-exports",
-    "all",
-    "never",
-  },
+
+local PLAYS = {
+  SETUP_DESKTOP = "10-10 [Setup desktop]",
+  SETUP_KEYBOARD_TOOLS = "10-40 [Setup keyboard tools]",
+  SETUP_WINE_TOOLS = "10-120 [Setup wine tools]",
+  SETUP_BROWSER_TOOLS = "10-170 [Setup browser tools]",
+  SETUP_BACKUP_TOOLS = "10-180,10-181 [Setup backup tools]",
+  SETUP_COMPOSITOR = "10-100 [Setup compositor]",
+  SETUP_DOTFILES = "10-30 [Setup dotfiles]",
+  SETUP_VIRTUALBOX = "10-140 [Setup virtualbox]",
+  SETUP_PERSISTENCE_TOOLS = "10-150 [Setup persistence tools]",
+  SETUP_OTHER_TOOLS = "10-160 [Setup other tools]",
+  SETUP_NEOVIM = "20-50 [Setup neovim]",
+  SETUP_EDITOR_TOOLS = "20-60 [Setup editor tools]",
+  SETUP_FULL_EDITOR = "20-50,20-60 [Setup full editor + plugins]",
+  SETUP_MARKDOWN_TOOLS = "30-100 [Setup markdown tools]",
+  SETUP_HPI = "40-20 [Setup HPI]",
+  SETUP_EXPORTS = "40-30 [Setup exports]",
+  SETUP_AI_TOOLS = "20-170 [Setup AI tools]",
+  SETUP_CODE_GENERATION = "30-999 [Setup Code Generation]",
+  SETUP_ALL_NODE = "20-10,20-50,20-60,20-170,30-30,30-100,30-999 [Setup all node]", -- safe node playbook execution
+  ALL = "all",
+  NEVER = "never",
+  FOR_MY_EYES_ONLY_60_10 = "60-10 [for-my-eyes-only]",
+  FOR_MY_EYES_ONLY_60_20 = "60-20 [for-my-eyes-only]",
+  FOR_MY_EYES_ONLY_60_30 = "60-30 [for-my-eyes-only]",
+  FOR_MY_EYES_ONLY_60_40 = "60-40 [for-my-eyes-only]",
+  FOR_MY_EYES_ONLY_70_10 = "70-10 [for-my-eyes-only]",
+  ALWAYS = "always",
 }
 
 ---Maps os names to sshd_config names
@@ -54,7 +48,7 @@ return {
     task_id = {
       desc = "Task id",
       type = "enum",
-      choices = CHOICES[CWD] or {},
+      choices = vim.tbl_values(PLAYS),
       optional = false,
       order = 1,
     },
@@ -75,7 +69,7 @@ return {
     skip_tags = {
       desc = "Ignore the ansible always tag",
       type = "enum",
-      choices = { "always", "never" },
+      choices = { PLAYS.ALWAYS, PLAYS.NEVER },
       optional = true,
       order = 4,
     },
@@ -97,7 +91,7 @@ return {
       "--tags",
       utils.wrap_in_single_quotes(
         vim.fn.join(
-          vim.tbl_map(function(item) return item:gsub("([^-]*-[^-]*).*", "%1") end, vim.fn.split(params.task_id, ",")),
+          vim.tbl_map(function(item) return (item:match("^(%d+-%d+)") or item) end, vim.fn.split(params.task_id, ",")),
           ","
         )
       ),
