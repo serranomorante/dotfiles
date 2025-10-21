29dd593a584338d59f9d247d19b5724c9e9e0bdd
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-10-21 14:35:18 +0200

    chore(nvim): safer '0 override

--- nvim/dot-config/nvim/lua/serranomorante/remap.lua
+++ nvim/dot-config/nvim/lua/serranomorante/remap.lua
@@ -198,8 +198,8 @@ vim.keymap.set("n", "'0", function()
   local numbered_marks = { "'0", "'1", "'2", "'3", "'4", "'5", "'6", "'7", "'8", "'9" }
   for _, m in ipairs(vim.fn.getmarklist()) do
     if vim.list_contains(numbered_marks, m.mark) and utils.file_inside_cwd(m.file) and not utils.cwd_is_home() then
-      vim.cmd.normal({ args = { m.mark }, bang = true })
-      vim.cmd.normal({ "zz", bang = true })
+      local ok, _ = pcall(vim.cmd.normal, { args = { m.mark }, bang = true }) -- pcall because it randomly fails now...
+      if ok then vim.schedule(function() vim.cmd.normal({ "zz", bang = true }) end) end
       return
     end
   end
