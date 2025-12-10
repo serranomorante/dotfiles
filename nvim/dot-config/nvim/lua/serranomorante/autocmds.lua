eafa784f8e56c04426122472a8dab04fd3efe9ee
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-12-10 14:44:12 +0100

    chore(nvim): add rst syntax highlighting

--- nvim/dot-config/nvim/lua/serranomorante/autocmds.lua
+++ nvim/dot-config/nvim/lua/serranomorante/autocmds.lua
@@ -150,7 +150,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 local treesitter_filetypes = utils.ts_compatible_filetypes()
-local regex_filetypes = { "qf", "html", "remind", "spajson", "log", "pager" }
+local regex_filetypes = { "qf", "html", "remind", "spajson", "log", "pager", "rst" }
 vim.api.nvim_create_autocmd("FileType", {
   desc = "Enable syntax highlighting",
   group = vim.api.nvim_create_augroup("ts_highlighting", { clear = true }),
